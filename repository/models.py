5237b7f0ce525acbe8a88e393244938679b9c542
Balasankar C <balasankarc@autistici.org>
2015-09-13 00:36:14 +0530

    Add homepage

--- repository/models.py
+++ repository/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+# class Department(models.model):
+    # name = models.CharField(max_length=50)
+
+
+# class User(models.model):
+    # username = models.CharField(max_length=25)
+    # password = models.CharFiled()
+    # name = models.CharField(max_length=50)
+    # status = models.CharField(max_length=20)
+    # department = models.ForeignKey(Department)
+
+
+# class Subject(models.model):
+    # code = models.CharField(max_length=10)
+    # name = models.CharField(max_length=50)
+    # credit = models.IntegerField()
+    # course = models.CharField(max_length=10)
+    # department = models.ForeignKey(Department)
+
+
+# class Resource(models.model):
+    # title = models.CharField(max_length=100)
+    # category = models.CharField(max_length=50)
+    # subject = models.ForeignKey(Subject)
--- repository/views.py
+++ repository/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+
+def home(request):
+    content = "This is the homepage"
+    return render_to_response('home.html', {'content': content})
