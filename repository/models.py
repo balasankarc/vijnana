3ad771cbf379854d151892f4e2add504b5666055
Balasankar C <balasankarc@autistici.org>
2015-09-17 00:21:56 +0530

    Add basic classes and relations

--- repository/models.py
+++ repository/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
 
-# class Department(models.model):
-    # name = models.CharField(max_length=50)
+class Department(models.Model):
+    name = models.CharField(max_length=50)
 
 
-# class User(models.model):
-    # username = models.CharField(max_length=25)
-    # password = models.CharFiled()
-    # name = models.CharField(max_length=50)
-    # status = models.CharField(max_length=20)
-    # department = models.ForeignKey(Department)
+class User(models.Model):
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+    department = models.ForeignKey(Department)
 
 
-# class Subject(models.model):
-    # code = models.CharField(max_length=10)
-    # name = models.CharField(max_length=50)
-    # credit = models.IntegerField()
-    # course = models.CharField(max_length=10)
-    # department = models.ForeignKey(Department)
+class Subject(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    credit = models.IntegerField()
+    course = models.CharField(max_length=10)
+    semester = models.CharField(max_length=10)
+    department = models.ForeignKey(Department)
 
 
 # class Resource(models.model):
