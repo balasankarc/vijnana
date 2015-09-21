9c9815565843a9f59832eccdae9714fd57f34102
Balasankar C <balasankarc@autistici.org>
2015-09-21 17:12:41 +0530

    Forms for file upload

--- repository/forms.py
+++ repository/forms.py
@@ -11,3 +11,10 @@ class SignUpForm(forms.Form):
     password = forms.CharField()
     fullname = forms.CharField()
     department = forms.CharField()
+
+
+class NewResourceForm(forms.Form):
+    title = forms.CharField()
+    category = forms.CharField()
+    subject = forms.CharField()
+    resourcefile = forms.FileField()
