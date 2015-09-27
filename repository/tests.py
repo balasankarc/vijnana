b97e800e818d15d53475209e4e5d593dbb8be529
Balasankar C <balasankarc@autistici.org>
2015-09-27 18:26:09 +0530

    Basic route tests

--- repository/tests.py
+++ repository/tests.py
@@ -1,9 +1,11 @@
-from django.test import TestCase
-from .models import User, Subject
 from django.db import IntegrityError
+from django.test import TestCase
+
+from .models import Subject, User
 
 
 class UserTests(TestCase):
+
     def test_user_department_not_null(self):
         first_user = User(username='testuser1')
         with self.assertRaises(IntegrityError):
@@ -16,8 +18,35 @@ class UserTests(TestCase):
             second_user = User(username='testuser2', department_id=1)
             second_user.save()
 
+    def test_user_signup_missing_field(self):
+        response = self.client.post("/sign_up/",
+                                    {'username': 'testuser3',
+                                     'fullname': 'Test User 3',
+                                     'department': '1'})
+        self.assertTemplateUsed(response, 'signup.html')
+
+    def test_user_signup_and_signout_successfull(self):
+        response = self.client.post("/sign_up/",
+                                    {'username': 'testuser3',
+                                     'password': 'testuser3',
+                                     'fullname': 'Test User 3',
+                                     'department': '1'})
+        self.assertRedirects(response, '/')
+        self.assertEqual(self.client.session['user'], 'testuser3')
+        user = User.objects.get(id=1)
+        self.assertEqual(user.status, 'student')
+        response1 = self.client.get('/sign_out/')
+        self.assertRedirects(response1, '/')
+        self.assertNotIn('user', self.client.session)
+        response2 = self.client.post("/sign_in/",
+                                     {'username': 'testuser3',
+                                      'password': 'testuser3'})
+        self.assertRedirects(response2, '/')
+        self.assertEqual(self.client.session['user'], 'testuser3')
+
 
 class SubjectTests(TestCase):
+
     def test_subject_required_not_null(self):
         first_subject = Subject(code='subject1')
         with self.assertRaises(IntegrityError):
@@ -30,12 +59,6 @@ class SubjectTests(TestCase):
             second_subject = Subject(code='testsubject2', department_id=1)
             second_subject.save()
 
-
-class LinkTests(TestCase):
-    def test_homepage(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
     def test_subject_exists(self):
         first_subject = Subject(code='testsubject2', department_id=1)
         first_subject.save()
@@ -45,3 +68,11 @@ class LinkTests(TestCase):
     def test_subject_not_exists(self):
         response = self.client.get('/subject/1/')
         self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, 'error.html')
+
+
+class LinkTests(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
